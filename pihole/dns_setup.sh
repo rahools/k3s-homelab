ad79ba0f4ae0f80d42b7b4bb1b17daa1e4b64f2a
Rahul Singh <rahools@homelab.homelab.rahools.com>
2025-09-11 22:28:20 +0530

    feat: migrating to docker

--- pihole/dns_setup.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install resolvconf -y
-echo "nameserver 1.1.1.1" >> /etc/resolvconf/resolv.conf.d/head
-sudo systemctl start resolvconf
-sudo systemctl enable resolvconf
-sudo resolvconf -u
